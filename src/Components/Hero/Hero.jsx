112c7ca0b85f44449cfbef024b25e2dc9be7f414
raineer <raineerdelarita@gmail.com>
2024-10-29 15:53:55 -0700

    profile img

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,6 +7,12 @@ const Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img} alt="" />
+        <h1><span>I'm Raineer Dela Rita,</span>frontend developer Cebu, PHI with 5 years of experience</h1>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
+
     </div>
   )
 }
