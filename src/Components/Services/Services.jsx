4416d69d54ec44e14bf86bd7b6e17ce12ac5e75b
raineer <raineerdelarita@gmail.com>
2024-10-31 12:35:41 -0700

    arrow icon

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import Services_Data from '../../assets/services_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
 
 const Services = () => {
   return (
@@ -9,8 +11,19 @@ const Services = () => {
             <h1>My Services</h1>
             <img src={theme_pattern} alt="" />
         </div>
-        <div className="service-container">
-            
+        <div className="services-container">
+            {Services_Data.map((service, index) => {
+                return <div key={index} className='servies-format'>
+                    <h3>{service.s_no}</h3>
+                    <h2>{service.s_name}</h2>
+                    <p>service.s_desc</p>
+                    <div className="services-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_icon} alt="" />
+                    </div>
+                </div>
+                
+            })}
         </div>
     </div>
   )
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -3,5 +3,35 @@ const Services_Data = [
         s_no:"01",
         s_name:"Web design",
         s_desc:"Web development is the process of building, programming..."
-    }
+    },
+    {
+        s_no:"01",
+        s_name:"Web design",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics design",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"03",
+        s_name:"Social Media",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"04",
+        s_name:"App design",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"05",
+        s_name:"Digital Marketing",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"06",
+        s_name:"Content Writing",
+        s_desc:"Web development is the process of building, programming..."
+    },
 ]
