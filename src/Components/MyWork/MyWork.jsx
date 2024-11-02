507c9a59c4ed3d7e38835adcc626c62846fc6f45
raineer <raineerdelarita@gmail.com>
2024-11-01 18:15:14 -0700

    mywork

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MyWork.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+
+const MyWork = () => {
+  return (
+    <div className='myWork'>
+        <div className="mywork-title">
+            <h1>My latest work</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default MyWork
