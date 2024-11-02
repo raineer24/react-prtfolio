3b6d638b4f4ee0ec8d17f54f7a2fd96b7416c54c
raineer <raineerdelarita@gmail.com>
2024-11-02 16:32:37 -0700

    footer subscribe

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,8 +8,17 @@ const Footer = () => {
         <div className="footer-top">
             <div className="footer-top-left">
                 <img src={footer_logo} alt="" />
+                <p>I am a frontend developer from, Philippines with 5 years of experience.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={} alt="" />
+                    <input type="email" placeholder='Enter your email' />
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
             </div>
         </div>
+        <hr />
     </div>
   )
 }
