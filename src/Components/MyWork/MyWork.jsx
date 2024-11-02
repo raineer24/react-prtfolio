7f8da54ace48a7db1f06059a5d98e062b2d13255
raineer <raineerdelarita@gmail.com>
2024-11-01 19:20:42 -0700

    proj assets

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MyWork.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
+import mywork_data from '../../assets/mywork_data';
 
 const MyWork = () => {
   return (
@@ -8,6 +9,11 @@ const MyWork = () => {
         <div className="mywork-title">
             <h1>My latest work</h1>
             <img src={theme_pattern} alt="" />
+        </div>
+        <div className="mywork-container">
+            {mywork_data.map((work, index) => {
+                return <img key={index} src={work.w_img} alt="" />
+            })}
         </div>
     </div>
   )
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,41 @@
+import project1_img from '../assets/proj1.svg'
+import project2_img from '../assets/proj2.svg'
+import project3_img from '../assets/proj3.svg'
+import project4_img from '../assets/proj4.svg'
+import project5_img from '../assets/proj5.svg'
+import project6_img from '../assets/proj6.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img: project1_img
+    },
+]
+
+export default mywork_data;
