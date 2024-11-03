cf62b20c089086f81b3299de180127bdf0635aad
raineer <raineerdelarita@gmail.com>
2024-11-02 18:10:11 -0700

    footer subscribe:hover

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import  './Footer.css';
 import footer_logo from '../../assets/footer_logo.jpg';
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
@@ -12,13 +13,21 @@ const Footer = () => {
             </div>
             <div className="footer-top-right">
                 <div className="footer-email-input">
-                    <img src={} alt="" />
+                    <img src={user_icon} alt="" />
                     <input type="email" placeholder='Enter your email' />
                 </div>
                 <div className="footer-subscribe">Subscribe</div>
             </div>
         </div>
         <hr />
+        <div className="footer-bottom">
+            <p className="footer-bottom-left">© 2024 Raineer dela Rita. All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term of Services</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
     </div>
   )
 }
