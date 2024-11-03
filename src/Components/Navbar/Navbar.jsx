11d07e8e670ebfa64ff0d2f7621769a7f7223201
raineer <raineerdelarita@gmail.com>
2024-11-02 18:28:58 -0700

    useState menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.svg'
+import underline from '../../assets/nav.svg'
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("home")
   return (
     <div className='navbar'>
         <img src={logo} alt="" />
         <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
+            <li><p>Home</p>{menu==="home"? <img src={underline} alt='' />:<></>}</li>
+            <li><p></p>About Me</li>
+            <li><p></p>Services</li>
+            <li><p></p>Portfolio</li>
+            <li><p></p>Contact</li>
         </ul>
         <div className="nav-connect">Connect with Me</div>
     </div>
