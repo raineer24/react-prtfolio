d8ecb5a31b7f413876242a95913d848fd86aadb9
raineer <raineerdelarita@gmail.com>
2025-03-24 11:21:55 +0800

    changes portfolio: added portfolio imgs

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,24 +4,27 @@ import project3_img from '../assets/proj3.svg'
 import project4_img from '../assets/proj4.svg'
 import project5_img from '../assets/proj5.svg'
 import project6_img from '../assets/proj6.svg'
+import tf from '../assets/1.png';
+import tf1 from '../assets/2.png';
+import tf2 from '../assets/3.png';
 
 const mywork_data = [
     {
         url: "https://pern-stack-dztv.onrender.com/",
         w_no:1,
         w_name: "Web design",
-        w_img: project1_img
+        w_img: tf
     },
     {
         w_no:1,
         w_name: "Web design",
-        w_img: project2_img,
+        w_img: tf1,
         url: "https://rain-store.vercel.app/"
     },
     {
         w_no:1,
         w_name: "Web design",
-        w_img: project3_img,
+        w_img: tf2,
         url: "https://network-social.vercel.app/"
     },
     // {
